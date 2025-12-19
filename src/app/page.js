f35e4856277c43c8c745ef8fe9011c5ea95b3b26
Giridharmathavaraj <giridharmathavaraj@gmail.com>
2025-12-19 20:14:56 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <Link href="index.html">About</Link>
 //     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 //       <marquee direction="left"><h1>Hi To All</h1></marquee>
 //  <marquee direction="right"><h1>Hi To All</h1></marquee>
 //     </div>
-""
+
   );
 }
