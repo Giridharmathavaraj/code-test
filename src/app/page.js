855bcd780c648a1a985411d090718061725ac3fd
Giridharmathavaraj <giridharmathavaraj@gmail.com>
2025-12-19 20:42:45 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <Link href="index.html">About</Link>
+    <Link href="/index.html">About</Link>
 //     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 //       <marquee direction="left"><h1>Hi To All</h1></marquee>
 //  <marquee direction="right"><h1>Hi To All</h1></marquee>
